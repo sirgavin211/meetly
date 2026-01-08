faa6ceb3aa2465d39bddc80bf0951e0737f86afa
sirgavin211 <gtxudi@gmail.com>
2026-01-07 23:32:10 -0800

    finish hangout_name logic thinking abt how to implement ui for multiple locations

--- frontend/src/pages/CreateRoom/CreateHangout.jsx
+++ frontend/src/pages/CreateRoom/CreateHangout.jsx
@@ -6,6 +6,7 @@ import { useProfile } from "../../data/useProfile";
 
 export default function CreateHangout(){
 
+    //hangout data json structure
     const [hangout_data, setData] = useState({
         name: "",
         date: "",
@@ -14,13 +15,23 @@ export default function CreateHangout(){
         include_host: false,
     });
 
+    //get current profile data
     const profile = useProfile();
+    //numbers to keep track of each parameter
     const [form_parameter, setFormParameter] = useState(0);
+
+    //to check if they filled out their profile
     const [profileFilled, setFilled] = useState(false);
+
+    //the checkbox
+    const [includeHost, setIncludeHost] = useState(false);
+
+    //store data in the input text
+    const [parameter, setParameter] = useState("");
 
     function continueForm(data){
         
-        if(profile.first_name == "" || profile.last_name == "" || profile.age <=5){
+        if(profile.first_name === "" || profile.last_name === "" || profile.age <= 5 || profile.first_name === "Guest"){
             setFilled(true);
             return;
         }
@@ -54,26 +65,30 @@ export default function CreateHangout(){
                     <section className="form">
                         <div className="card">
                             {(form_parameter == 0) ? <label>What's your hangout?</label> : ""}
-                            <input type="text" placeholder="Enter your hangout name"/>
+                            <input type="text" placeholder="Enter your hangout name" value={parameter} onChange={(e) => setParameter(e.target.value)}/>
 
                                 <label className="checkboxLabel">
                                 <input
                                     type="checkbox"
-                                    // checked={enabled}
-                                    // onChange={(e) => {
-                                    //     setEnabled(e.target.checked)
-                                    //     updateHasFamily(e.target.checked);
-                                    // }}
+                                    checked={includeHost}
+                                    onChange={(e) => {
+                                        setIncludeHost(e.target.checked)
+                                    }}
                                 />
                                 Include me (the host) in the hangout
                             </label>
 
                             <div className="line"></div>
                             <div className="bar">
-                                <input type="button" className="continue_button" value="Continue" onClick={continueForm()}></input>
+                                <input type="button" className="continue_button" value="Continue" onClick={continueForm}></input>
                             </div>
 
-                            <small className="caption">A memorable name for your hangout.</small>
+                            {
+                                profileFilled ? 
+                                <small className="warning">Fill out your profile first please!</small>
+                                :
+                                <small className="caption">A memorable name for your hangout.</small>
+                            }
                         </div>
                     </section>
                 </div>
