b683711d012577baee96fa998a0069a510c502aa
sirgavin211 <gtxudi@gmail.com>
2026-01-11 22:40:55 -0800

    fixed directory bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home/Home.jsx";
 import Profile from "./pages/Profile/Profile.jsx";
-import CreateHangout from './pages/CreateRoom/CreateHangout.jsx';
+import CreateHangout from './pages/CreateHangout/CreateHangout.jsx';
 
 function App() {
-  
+
 
   return (
     <Routes>
